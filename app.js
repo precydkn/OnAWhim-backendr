12ab8a3f3a0079686d8ae00e23d27a5749ee4ee1
precydkn <diokno.precy@gmail.com>
2025-11-26 11:42:47 +0400

    Updated for render

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
-const path = require('path');
 const User = require('./models/user');
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
+const FRONTEND_URL = process.env.FRONTEND_URL; // oaw render frontend
+const MONGODB_URI = process.env.MONGODB_URI; // atlas connection string
 
 const app = express();
-const port = 3300;
+const port = process.env.PORT || 3300; // for render port
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(cors({
+    origin: FRONTEND_URL || '*',
+    credentials: true
+}));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/onawhim')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // Routes
 const users = require('./routes/users');
 app.use('/users', users);
-
-// Admin routes
-// app.use('/admin', auth, express.static(path.join(__dirname, '../frontend/admin')));
 
 // Login route
 app.post('/login', async (req, res) => {
